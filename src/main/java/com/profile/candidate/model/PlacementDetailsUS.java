53445e28d4eb187f228bcf6c0b515a66f17e5057
venugopaaladroit-collab <venugopaal.adroit@gmail.com>
2026-05-14 13:17:08 +0530

    fix accepting dec values

--- src/main/java/com/profile/candidate/model/PlacementDetailsUS.java
+++ src/main/java/com/profile/candidate/model/PlacementDetailsUS.java
@@ -108,7 +108,7 @@ public class PlacementDetailsUS {
     private String userId;
 
     @Column(name = "hold_rate")
-    private Integer holdRate;
+    private String holdRate;
 
     @Column(name = "referal")
     private String referal;
@@ -124,6 +124,270 @@ public class PlacementDetailsUS {
 
     @Column(name = "currency")
     private String currency;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCandidateFullName() {
+        return candidateFullName;
+    }
+
+    public void setCandidateFullName(String candidateFullName) {
+        this.candidateFullName = candidateFullName;
+    }
+
+    public String getCandidateContactNo() {
+        return candidateContactNo;
+    }
+
+    public void setCandidateContactNo(String candidateContactNo) {
+        this.candidateContactNo = candidateContactNo;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public String getCandidateEmailId() {
+        return candidateEmailId;
+    }
+
+    public void setCandidateEmailId(String candidateEmailId) {
+        this.candidateEmailId = candidateEmailId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getRecruiterName() {
+        return recruiterName;
+    }
+
+    public void setRecruiterName(String recruiterName) {
+        this.recruiterName = recruiterName;
+    }
+
+    public String getSales() {
+        return sales;
+    }
+
+    public void setSales(String sales) {
+        this.sales = sales;
+    }
+
+    public String getBillRate() {
+        return billRate;
+    }
+
+    public void setBillRate(String billRate) {
+        this.billRate = billRate;
+    }
+
+    public String getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(String payRate) {
+        this.payRate = payRate;
+    }
+
+    public String getGrossProfit() {
+        return grossProfit;
+    }
+
+    public void setGrossProfit(String grossProfit) {
+        this.grossProfit = grossProfit;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getInterviewId() {
+        return interviewId;
+    }
+
+    public void setInterviewId(String interviewId) {
+        this.interviewId = interviewId;
+    }
+
+    public Boolean getRegister() {
+        return isRegister;
+    }
+
+    public void setRegister(Boolean register) {
+        isRegister = register;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public BigDecimal getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(BigDecimal hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public String getEmployeeWorkingType() {
+        return employeeWorkingType;
+    }
+
+    public void setEmployeeWorkingType(String employeeWorkingType) {
+        this.employeeWorkingType = employeeWorkingType;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getHoldRate() {
+        return holdRate;
+    }
+
+    public void setHoldRate(String holdRate) {
+        this.holdRate = holdRate;
+    }
+
+    public String getReferal() {
+        return referal;
+    }
+
+    public void setReferal(String referal) {
+        this.referal = referal;
+    }
+
+    public String getProjectIn() {
+        return projectIn;
+    }
+
+    public void setProjectIn(String projectIn) {
+        this.projectIn = projectIn;
+    }
+
+    public String getVisa() {
+        return visa;
+    }
+
+    public void setVisa(String visa) {
+        this.visa = visa;
+    }
+
+    public String getProjectInC2cSubVendorName() {
+        return projectInC2cSubVendorName;
+    }
+
+    public void setProjectInC2cSubVendorName(String projectInC2cSubVendorName) {
+        this.projectInC2cSubVendorName = projectInC2cSubVendorName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 
     @PrePersist
     public void prePersist() {
