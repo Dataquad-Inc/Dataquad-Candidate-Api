7ef735ad55091fa1f307be36669d4ef0833e504f
S-Amulya000 <siddipetaamulyareddy1903@gmail.com>
2026-06-09 15:11:51 +0530

    sending test mail to bench candidates

---
+++ src/main/java/com/profile/candidate/dto/SendJdRequestDto.java
@@ -0,0 +1,34 @@
+package com.profile.candidate.dto;
+
+import java.util.List;
+
+public class SendJdRequestDto {
+
+    private List<String> emails;
+    private String subject;
+    private String body;
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
